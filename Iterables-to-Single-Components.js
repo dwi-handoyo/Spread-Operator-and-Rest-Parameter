6485170c5c2038ff58125fe1ea2a3e8b4d5a7a0f
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-06-30 22:38:39 +0700

    Create Iterables-to-Single-Components.js

---
+++ Iterables-to-Single-Components.js
@@ -0,0 +1,14 @@
+
+// Breaking Array or Any Iterables Into Single Components
+
+const mhs = ["Michael", "Bobby", "Jackson"];
+
+console.log(...mhs);
+
+//output: "Michael", "Bobby", "Jackson"
+
+console.log(...mhs[0]);
+
+//output: "M", "i", "c", "h", "a", "e", "l"
+
+
