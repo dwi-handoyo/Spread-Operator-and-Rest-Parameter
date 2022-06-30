b069934fa2c322c1b415f71c39b6ae9554b928e8
Dwi Handoyo <107865228+dwi-handoyo@users.noreply.github.com>
2022-06-30 23:26:00 +0700

    Create Pass-Parameters-To-Function.js

---
+++ Pass-Parameters-To-Function.js
@@ -0,0 +1,15 @@
+// Pass an indefinite number of parameters to a function using Rest Parameters.
+
+function fun(...input){
+    let sum = 0;
+    for(let i of input){
+        sum+=i;
+    }
+    return sum;
+}
+
+console.log(fun(1,2)); //3
+console.log(fun(1,2,3)); //6
+console.log(fun(1,2,3,4,5)); //15   
+
+
